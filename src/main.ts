2c1ca18b1837aa9e9521d724df44e24450ca1054
Pascal Jedicke <pascal.jedicke@icloud.com>
2025-01-08 15:03:24 +0100

    feat: added pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import './assets/index.css';
 import router from "@/router.ts";
 
 const app = createApp(App);
+const pinia = createPinia()
 
 app.use(router);
+app.use(pinia);
 
 // Mount the app to DOM
 app.mount("#app");
