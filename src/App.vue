362cf6c601b36ea403349c1a86e9d3c0e911139f
Pascal Jedicke <pascal.jedicke@icloud.com>
2025-01-09 21:17:47 +0100

    feat: added new Attributions Dialog

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,15 @@ import { Separator } from "@/components/ui/separator";
 import { Map, House, TriangleAlert, Info } from 'lucide-vue-next';
 import { useFintrafficStore } from "@/stores/fintraffic.store.ts";
 import SidebarFooter from './components/ui/sidebar/SidebarFooter.vue';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 
 const fintrafficStore = useFintrafficStore();
 const lastUpdate = ref('');
@@ -69,87 +78,110 @@ async function routeTo(link: string) {
 </script>
 
 <template>
-  <SidebarProvider>
-    <Sidebar collapsible="icon">
-      <!-- Sidebar Header Config, Apptitle and Searchbar for Trains, Stations or Operators -->
-      <SidebarHeader>
-        <SidebarMenuButton
-            size="lg"
-            class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-        >
-          <Avatar class="h-8 w-8 rounded-lg">
-            <AvatarImage src="/vue.svg" alt="RailtrackLogo" />
-            <AvatarFallback class="rounded-lg">
-              RT
-            </AvatarFallback>
-          </Avatar>
-          <div class="grid flex-1 text-left text-sm leading-tight">
-            <span class="truncate font-semibold">Railtrack</span>
-            <span class="truncate text-xs font-thin">Version: 0.1.0</span>
-          </div>
-        </SidebarMenuButton>
-      </SidebarHeader>
+  <Dialog>
+    <SidebarProvider>
+      <Sidebar collapsible="icon">
+        <!-- Sidebar Header Config, Apptitle and Searchbar for Trains, Stations or Operators -->
+        <SidebarHeader>
+          <SidebarMenuButton
+              size="lg"
+              class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
+            <Avatar class="h-8 w-8 rounded-lg">
+              <AvatarImage src="/vue.svg" alt="RailtrackLogo" />
+              <AvatarFallback class="rounded-lg">
+                RT
+              </AvatarFallback>
+            </Avatar>
+            <div class="grid flex-1 text-left text-sm leading-tight">
+              <span class="truncate font-semibold">Railtrack</span>
+              <span class="truncate text-xs font-thin">Version: 0.1.0</span>
+            </div>
+          </SidebarMenuButton>
+        </SidebarHeader>
 
-      <!-- Sidebar Content Config, Links to Livemap, Servicedisruptions and more -->
-      <SidebarContent>
-        <SidebarGroup>
+        <!-- Sidebar Content Config, Links to Livemap, Servicedisruptions and more -->
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton @click="routeTo('/stations')">
+                  <House/>
+                  <span>Stations</span>
+                </SidebarMenuButton>
+                <SidebarMenuButton @click="routeTo('/map')">
+                  <Map/>
+                  <span>Map</span>
+                </SidebarMenuButton>
+                <SidebarMenuButton @click="routeTo('/disruptions')">
+                  <TriangleAlert/>
+                  <span>Disruptions</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton @click="routeTo('/stations')">
-                <House/>
-                <span>Stations</span>
-              </SidebarMenuButton>
-              <SidebarMenuButton @click="routeTo('/map')">
-                <Map/>
-                <span>Map</span>
-              </SidebarMenuButton>
-              <SidebarMenuButton @click="routeTo('/disruptions')">
-                <TriangleAlert/>
-                <span>Disruptions</span>
-              </SidebarMenuButton>
+              <DialogTrigger asChild>
+                <SidebarMenuButton>
+                  <Info/>
+                  <span>Attributions</span>
+                </SidebarMenuButton>
+              </DialogTrigger>
             </SidebarMenuItem>
           </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton>
-              <Info/>
-              <span>Attributions</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
-      <SidebarRail/>
-    </Sidebar>
+        </SidebarFooter>
+        <SidebarRail/>
+      </Sidebar>
 
-    <!-- Sidebar Inset Config, Routerview and Nav -->
-    <SidebarInset>
-      <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        <SidebarTrigger class="-ml-1"/>
-        <Separator orientation="vertical" class="mr-2 h-4"/>
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem class="hidden md:block">
-              <BreadcrumbLink>
-                Railtrack
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator class="hidden md:block"/>
-            <BreadcrumbItem class="hidden md:block">
-              <BreadcrumbPage>
-                {{ pageName }}
-              </BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-        <div class="relative ml-auto md:grow-0">
-          <Label class="text-xs font-thin">Last Refreshed: {{ lastUpdate }}</Label>
+      <!-- Sidebar Inset Config, Routerview and Nav -->
+      <SidebarInset>
+        <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger class="-ml-1"/>
+          <Separator orientation="vertical" class="mr-2 h-4"/>
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem class="hidden md:block">
+                <BreadcrumbLink>
+                  Railtrack
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator class="hidden md:block"/>
+              <BreadcrumbItem class="hidden md:block">
+                <BreadcrumbPage>
+                  {{ pageName }}
+                </BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <div class="relative ml-auto md:grow-0">
+            <Label class="text-xs font-thin">Last Refreshed: {{ lastUpdate }}</Label>
+          </div>
+        </header>
+        <RouterView />
+      </SidebarInset>
+    </SidebarProvider>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Attributions</DialogTitle>
+        <DialogDescription>Other Software and Data used to create Railtrack</DialogDescription>
+      </DialogHeader>
+      <div class="grid gap-4 py-4 overflow-y-auto px-6">
+        <div class="flex flex-col justify-between">
+          <p>
+            &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>,
+            Style: <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA 2.0</a>
+          </p>
+          <a href="http://www.openrailwaymap.org/">OpenRailwayMap</a>
+          <a href="http://www.openstreetmap.org/">OpenStreetMap</a>
+          <p>
+            Finland: <a href="https://www.digitraffic.fi/rautatieliikenne/">digitraffic.fi</a>
+          </p>
         </div>
-      </header>
-      <RouterView />
-    </SidebarInset>
-  </SidebarProvider>
+      </div>
+    </DialogContent>
   <Toaster class="pointer-events-auto" />
+  </Dialog>
 </template>
