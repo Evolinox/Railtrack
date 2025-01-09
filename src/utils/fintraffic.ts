b626e7e2448eef000ad0e0f2b98176f42f7bad4f
Pascal Jedicke <pascal.jedicke@icloud.com>
2025-01-09 11:14:01 +0100

    refactor(fintraffic): removed old function

--- src/utils/fintraffic.ts
+++ src/utils/fintraffic.ts
@@ -9,36 +9,9 @@ const fintrafficStore = useFintrafficStore();
 // API Endpoints
 const trainLocationsLatestUrl = 'https://rata.digitraffic.fi/api/v1/train-locations/latest/';
 
-// Returns a json with all important informations about a train in finland
+
 export async function getTrainPositions(): Promise<Train[] | undefined> {
-    // check store
-    if (fintrafficStore.trains.length === 0) {
-        // fetch new data
-        console.log("Requesting new data from fintraffic api")
-
-        const response = await fetch(trainLocationsLatestUrl);
-        if (response.ok) {
-            const trains = await response.json();
-            for (const train of trains) {
-                await fintrafficStore.addTrain(train);
-            }
-            return fintrafficStore.getTrains;
-        } else {
-            toast({
-                variant: 'destructive',
-                title: 'Uh oh! Something went wrong.',
-                description: 'There was an error. Code: ' + response.status,
-            });
-        }
-    } else {
-        // use already present data
-        console.log("Using stored data from fintraffic api")
-        return fintrafficStore.getTrains;
-    }
-}
-
-export async function getUpdatedTrainPositions(): Promise<Train[] | undefined> {
-    console.log("Requesting new position data from fintraffic api")
+    console.log("Requesting new position data from fintraffic api");
     const response = await fetch(trainLocationsLatestUrl);
     if (response.ok) {
         const trains = await response.json();
