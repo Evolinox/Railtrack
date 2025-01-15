5106213e88700c53dcf9ff4f77c91189e60a506a
Pascal Jedicke <pascal.jedicke@icloud.com>
2025-01-15 15:17:01 +0100

    feat(settings): clearLocalStorage also clears fintraffic train store

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import {useFintrafficStore} from '../stores/fintraffic.store.ts';
+
+const fintrafficStore = useFintrafficStore();
 
 function clearLocalStorage() {
-  localStorage.clear()
+  localStorage.clear();
+  fintrafficStore.clearTrains();
 }
 </script>
 
