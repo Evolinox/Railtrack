3acd1fe687b20d4c953195ee7f9b954a09989a43
Pascal Jedicke <pascal.jedicke@icloud.com>
2025-01-15 13:52:26 +0100

    feat(settings): added clear localstorage button

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 
+function clearLocalStorage() {
+  localStorage.clear()
+}
 </script>
 
 <template>
-settings
+  <div class="border-t px-4 pb-4">
+  </div>
+  <Button variant="destructive" @click="clearLocalStorage">
+    Clear Local Storage
+  </Button>
 </template>
 
 <style scoped>
