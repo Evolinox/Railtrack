dbf741521e0d74f5ac2d856ee138094ef18c5678
Pascal Jedicke <pascal.jedicke@icloud.com>
2025-01-20 01:31:24 +0100

    feat: added option to disable trafficrestriction markers

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import * as leaflet from 'leaflet';
-import {onMounted, onUnmounted} from 'vue';
+import {onMounted, onUnmounted, ref, watch} from 'vue';
 import {useColorMode} from "@vueuse/core";
+
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu'
 
 import { getTrainPositions, removeTrain, initializeMetadata, updateTrafficRestrictions } from '@/utils/fintraffic.ts';
 import {Train} from "@/utils/fintraffic.types.ts";
@@ -9,6 +20,9 @@ import {Train} from "@/utils/fintraffic.types.ts";
 const colorMode = useColorMode();
 //const { coords } = useGeolocation();
 const isDark = colorMode.value === 'dark';
+const mapType = ref('standard')
+const showDisruption = ref(false);
+const disruptionMarkers: leaflet.Marker[] = [];
 let map: leaflet.Map;
 
 const validateImage = (url: string, placeholder: string): Promise<string> => {
@@ -47,7 +61,6 @@ onMounted(async () => {
   // fintraffic/digitraffic
   const fintrafficMarkers = new Map();
   await initializeMetadata();
-  await addTrafficRestrictions(map);
   await refreshFintrafficMarker(map, fintrafficMarkers);
 
   // Intervals for live tracking
@@ -79,19 +92,34 @@ watch(() => coords.value, async() => {
   map.setView([coords.value.latitude, coords.value.longitude], 12);
 })
 */
-async function addTrafficRestrictions(map: leaflet.Map) {
-  const restIcon = leaflet.icon({
-    iconUrl: new URL(`../assets/restriction.webp`, import.meta.url).href,
-    iconSize: [32, 32],
-    iconAnchor: [16, 16],
-    popupAnchor: [0, -16],
-  });
-  const trafficRestrictions = await updateTrafficRestrictions();
-  for (const restriction of trafficRestrictions) {
-    const restMarker = leaflet.marker(restriction.location, {icon: restIcon}).addTo(map);
-    restMarker.bindPopup(`<b>Traffic Restriction</b><br>Organization: ${restriction.organization}<br>Starting: DD.MM.YY`);
+
+// Watch showDisruption and handle a change event
+watch(showDisruption, async (show, isShown) => {
+  console.log('showDisruption changed from', isShown, 'to', show);
+  // New state
+  if (show) {
+    // Create Marker Icon
+    const restIcon = leaflet.icon({
+      iconUrl: new URL(`../assets/restriction.webp`, import.meta.url).href,
+      iconSize: [32, 32],
+      iconAnchor: [16, 16],
+      popupAnchor: [0, -16],
+    });
+    // Get Traffic Restrictions
+    const trafficRestrictions = await updateTrafficRestrictions();
+    // Create Markers and add them to the Map
+    for (const restriction of trafficRestrictions) {
+      const restMarker = leaflet.marker(restriction.location, {icon: restIcon}).addTo(map);
+      restMarker.bindPopup(`<b>Traffic Restriction</b><br>Organization: ${restriction.organization}<br>Starting: DD.MM.YY`);
+      disruptionMarkers.push(restMarker);
+    }
+  } else {
+    // Delete all Markers
+    for (const marker of disruptionMarkers) {
+      map.removeLayer(marker);
+    }
   }
-}
+});
 
 async function refreshFintrafficMarker(map: leaflet.Map, fintrafficMarkers: Map<any, any>) {    // refresh positions
   console.log('refreshing positions');
@@ -168,7 +196,33 @@ async function refreshFintrafficMarker(map: leaflet.Map, fintrafficMarkers: Map<
 </script>
 
 <template>
-  <div id="map" v-bind:class="(isDark)?'dark':''" class="border h-full mr-2 mb-2 z-0"></div>
+  <ContextMenu>
+    <ContextMenuTrigger class="h-full pb-2">
+      <div id="map" v-bind:class="(isDark)?'dark':''" class="border h-full mr-2 mb-2 z-0"></div>
+    </ContextMenuTrigger>
+    <ContextMenuContent>
+      <ContextMenuRadioGroup v-model="mapType">
+        <ContextMenuLabel>
+          Map Type
+        </ContextMenuLabel>
+        <ContextMenuSeparator />
+        <ContextMenuRadioItem value="standard">
+          Standard
+        </ContextMenuRadioItem>
+        <ContextMenuRadioItem value="signals">
+          Signalling
+        </ContextMenuRadioItem>
+        <ContextMenuRadioItem value="maxspeed">
+          Speed
+        </ContextMenuRadioItem>
+      </ContextMenuRadioGroup>
+      <ContextMenuLabel>
+        Map Options
+      </ContextMenuLabel>
+      <ContextMenuSeparator />
+      <ContextMenuCheckboxItem v-model:checked="showDisruption">Show Disruptions</ContextMenuCheckboxItem>
+    </ContextMenuContent>
+  </ContextMenu>
 </template>
 
 <style>
